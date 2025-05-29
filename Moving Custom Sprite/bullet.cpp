cd53f90e615dfb4f6c0da0064ba729ba17b138db
UmbrellaCorp99 <ayoung6@mail.umw.edu>
2025-05-29 11:49:39 -0400

    Changed bullet into an 8x8 bitmap comprised of a red filled rectagle and a green filled circle

--- Moving Custom Sprite/bullet.cpp
+++ Moving Custom Sprite/bullet.cpp
@@ -1,8 +1,34 @@
 #include "bullet.h"
+
 bullet::bullet()
 {
 	alive = false;;
 	srand(time(0));
+	bullet_bmp[1] = NULL;
+}
+bullet::~bullet()
+{
+	al_destroy_bitmap(bullet_bmp[1]);
+}
+void bullet::drawBullet()
+{
+	al_draw_bitmap(bullet_bmp[1], getX(), getY(), 0);
+}
+void bullet::create_bullet_bitmap(ALLEGRO_DISPLAY* display)
+{
+	bullet_bmp[1] = al_create_bitmap(8, 8);
+	if (!bullet_bmp[1]) {
+		exit(1);
+		al_destroy_display(display);
+
+	}
+	al_set_target_bitmap(bullet_bmp[1]);
+	al_clear_to_color(al_map_rgb(0, 0, 0));
+
+	int x = 0;
+	int y = 0;
+	al_draw_filled_rectangle(x, y, x + 8, y + 8, al_map_rgb(255, 0, 0));
+	al_draw_filled_circle(x + 4, y + 4, 3, al_map_rgb(0, 255, 0));
 }
 bool bullet::getStatus()
 {
@@ -18,16 +44,19 @@ void bullet::fire()
 }
 void bullet::erase_bullet()
 {
-	al_draw_filled_rectangle(x,y,x+5,y+5,al_map_rgb(0,0,0)); //black color
+	al_draw_filled_rectangle(x, y, x + 8, y + 8, al_map_rgb(0, 0, 0));
+	al_draw_filled_circle(x + 4, y + 4, 3, al_map_rgb(0, 0, 0));
 }
 int bullet::move_bullet (int arrowX, int arrowY, int width, int length, int height)
 {
 	y++;
-	al_draw_filled_rectangle(x,y,x+5,y+5,al_map_rgb(255,255,0));//yellow color
+	al_draw_filled_rectangle(x, y, x + 8, y + 8, al_map_rgb(255, 0, 0));
+	al_draw_filled_circle(x + 4, y + 4, 3, al_map_rgb(0, 255, 0));
 
 
 	if (x > arrowX && x < arrowX+width && y > arrowY && y < arrowY+length) {
-		al_draw_filled_rectangle(x,y,x+5,y+5,al_map_rgb(0,0,0)); //BLACK
+		al_draw_filled_rectangle(x,y,x+8,y+8,al_map_rgb(0,0,0)); //BLACK
+		al_draw_filled_circle(x + 4, y + 4, 3, al_map_rgb(0, 0, 0));
 		alive=false;
 		return 1;
 	}
@@ -36,6 +65,14 @@ int bullet::move_bullet (int arrowX, int arrowY, int width, int length, int heig
 	return 0;
 
 
-} 
+}
+int bullet::getX()
+{
+	return x;
+}
+int bullet::getY()
+{
+	return y;
+}
 
 
--- Moving Custom Sprite/bullet.h
+++ Moving Custom Sprite/bullet.h
@@ -6,12 +6,18 @@ class bullet
 {
 public:
 	bullet();
+	~bullet();
+	void create_bullet_bitmap(ALLEGRO_DISPLAY* display);
 	void fire();
 	int move_bullet(int arrowX, int arrowY, int width, int length, int height);
 	void erase_bullet();
+	void drawBullet();
 	bool getStatus(); 
+	int getX();
+	int getY();
 private:
 	int x,y;
 	bool alive;
+	ALLEGRO_BITMAP* bullet_bmp[1];
 };
 
